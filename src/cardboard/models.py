2a6a9f1508ee39ba6a456f83f5daba6cd097e66c
David <irdof321@gmail.com>
2026-03-01 12:08:16 +0100

    models done

---
+++ src/cardboard/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+
+User = get_user_model()
+
+class Status(models.TextChoices):
+    TODO = 'todo', 'To Do'
+    IN_PROGRESS = 'in_progress', 'In Progress'
+    DONE = 'done', 'Done'
+
+class Priority(models.TextChoices):
+    LOW = 'low', 'Low'
+    MEDIUM = 'medium', 'Medium'
+    HIGH = 'high', 'High'
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boards')
+    members = models.ManyToManyField(User, related_name='member_boards')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Column(models.Model):
+    name = models.CharField(max_length=50)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='columns')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    order = models.IntegerField() # place in the board
+
+class Card(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.TODO)
+    priority = models.CharField(max_length=20, choices=Priority.choices, default=Priority.MEDIUM)
+    column = models.ForeignKey(Column, on_delete=models.CASCADE, related_name='cards')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    order = models.IntegerField() # place in the column
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_cards')
